e6afd2e397041da975ea6df2e3bbcb3f71ec792f
Gesa Rizky Nugraha <109656432+gesarizky@users.noreply.github.com>
2022-12-22 13:51:49 +0700

    Add files via upload

---
+++ kode.59.massa.py
@@ -0,0 +1,15 @@
+# Author  : Gesa Rizky Nugraha
+# Email   : gesarizkynugraha@gmail.com
+# Website : karenabelajar.blogspot.com
+
+# Menginput
+m = float(input("Masukan Nilai Massa Zat (m) : "))
+v = float(input("Masukan Nilai Volume Zat (v): "))
+
+# Mengkonversi
+p = m / v
+
+# Menampilkan Hasil
+print('==========================================================')
+print('Maka Nilai Massa Jenis (p) = ',round(p,3))
+print('==========================================================')
